4d8c8f3fb3e70c9f4f956eba24e985665bd8fe4e
Josh Fitzpatrick <jtfitzpa@umich.edu>
2022-12-02 06:33:43 -0500

    Day 2

--- aoc/days/day2/day2.py
+++ aoc/days/day2/day2.py
@@ -1,43 +1,89 @@
-# AOC 2022 Day 1
-import argparse
+# AOC 2022 Day 2
+from aocd import get_data
+
+OPPONENT_ACTIONS = {
+    'A': 1, # rock
+    'B': 2, # paper 
+    'C': 3, # scissors
+}
+MY_ACTIONS = {
+    'X': 1, # rock # lose
+    'Y': 2, # paper # draw
+    'Z': 3, # scissors # win
+}
 
 
-def read_file(filename):
-    with open(filename, 'r', encoding='UTF-8') as f:
-        input = f.readlines()
-    cleaned_input = []
-    for line in input:
-        no_return = line.rstrip('\n')
-        if no_return:
-            no_return = int(no_return)
-        else:
-            no_return = None
-        cleaned_input.append(no_return)
+def get_next_reward(opponent, me):
+    opponent_value = OPPONENT_ACTIONS[opponent]
+    my_value = MY_ACTIONS[me]
 
-    return cleaned_input
+    index = (my_value - opponent_value) % 3
 
-def group_items(input):
-    totals = [0]
-    for item in input:
-        if item:
-            totals[-1] += item
-        else:
-            totals.append(0)
+    if index == 0:
+        outcome = 3
 
-    return totals
+    elif index == 1:
+        outcome = 6 
+    
+    else:
+        outcome = 0
+    
+    return outcome + my_value
+
+def get_next_reward_2(opponent, me):
+    opponent_value = OPPONENT_ACTIONS[opponent]
+    my_value = MY_ACTIONS[me]
+
+    if my_value == 1: # lose
+        outcome = 0
+        action = (opponent_value - 1 - 1) % 3 + 1
+
+    elif my_value == 2:
+        outcome = 3
+        action = opponent_value
+
+    else:
+        outcome = 6 
+        action = opponent_value % 3 + 1
+    
+    return outcome + action
+
+
+
+def tournament(actions):
+    total = 0
+    for round in actions:
+        total += get_next_reward(*round)
+
+    return total
+
+def tournament2(actions):
+    total = 0
+    for round in actions:
+        total += get_next_reward_2(*round)
+
+    return total
+
+def format_input(input_str):
+    return  [l.split(' ') for l in input_str.split('\n')]
 
 def main():
-    ap = argparse.ArgumentParser()
-    ap.add_argument('file', type=str)
-    args = ap.parse_args()
 
+    # test = 'A Y\nB X\nC Z\nA X\nA Z\nB Y\nB Z\nC X\nC Y'
+    test = 'A Y\nB X\nC Z'
+    test_inputs = format_input(test)
+    reward1 = tournament(test_inputs)
+    reward = tournament2(test_inputs)
+    assert reward1 == 15
+    assert reward == 12
 
-    input = read_file(args.file)
-    result = group_items(input)
-    max_food = sorted(result)
+    data = get_data(day=2, year=2022)
+    inputs = format_input(data)
+    reward = tournament(inputs)
+    print(f'Total Reward: {reward}')
+    reward2 = tournament2(inputs)
+    print(f'Total Reward: {reward2}')
 
-    print(f'Final Answer - Part 1: {max(max_food)}')
-    print(f'Final Answer - Part 2: {sum(max_food[-3:])}')
 
 
 if __name__ == '__main__':
